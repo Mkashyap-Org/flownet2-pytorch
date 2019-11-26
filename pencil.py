00f12cf25ac32f4e96228651083b6e5fac15547a
MK <kashyap022@yahoo.in>
2019-11-26 09:29:43 +0100

    read from folder

--- pencil.py
+++ pencil.py
@@ -1,15 +1,19 @@
+#!/usr/local/bin/python3
+
 import cv2
+import os
+import glob
 import timeit
 import numpy as np
+from PIL import Image
 
-
-def fast_pencil(input_image, element):
-    gray = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
-    pencil_image = cv2.dilate(gray, element)
+#def fast_pencil(input_image, element):
+#    gray = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
+#    pencil_image = cv2.dilate(gray, element)
     # 65026 = 255**2 + 1 <=> the division will always be < 1.0 and thus casted to 0
-    pencil_image[pencil_image == 0] = 65026
+#    pencil_image[pencil_image == 0] = 65026
     # NEED to write 255.0 and not 255 because we multiply 2 uint8 and thus the value overflows
-    return np.expand_dims(255.0 * gray / pencil_image, axis=2).astype(np.uint8)
+#    return np.expand_dims(255.0 * gray / pencil_image, axis=2).astype(np.uint8)
 
 
 def orig_pencil(input_image, element):
@@ -30,31 +34,39 @@ def orig_pencil(input_image, element):
 
     pencil_image = valout.copy()
     pencil_image = np.expand_dims(pencil_image, axis=2)
-
     return pencil_image
 
 
 if __name__ == "__main__":
-    im = cv2.imread('something.jpg')
+    path = "C:\\Users\\Mahesh\\Desktop\\Kaiserslatern\\Sem3\\Project\\Imp\\FlyingChairs1_Samp\\"
+    penciled_path = "C:\\Users\\Mahesh\\Desktop\\Kaiserslatern\\Sem3\\Project\\Imp\\Penciled\\"
+    for file in os.listdir(path):
+        # c = cv2.imread(file)
+        im = Image.open(file)
+        print(file)
+        # im = cv2.cvtColor(c, cv2.COLOR_BGR2RGB)
+        # cv2.imshow('image', im)
+        # cv2.waitKey(0)
+        # NOTE: the 120 is a parameter which should be adapted to the input image size
+        dilate_size = int(im.shape[0] / 120)
+        kernel_size = 2*dilate_size + 1
 
-    # NOTE: the 120 is a parameter which should be adapted to the input image size
-    dilate_size = int(im.shape[0] / 120)
-    kernel_size = 2*dilate_size + 1
-
-    element = cv2.getStructuringElement(
+        element = cv2.getStructuringElement(
         cv2.MORPH_ELLIPSE, (kernel_size, kernel_size), (dilate_size, dilate_size))
 
-    fast = fast_pencil(im, element)
-    orig = orig_pencil(im, element)
-    print(np.array_equal(orig, fast))
+#    fast = fast_pencil(im, element)
+        orig = orig_pencil(im, element)
+#    print(np.array_equal(orig, fast))
 
-    times = timeit.repeat(lambda: fast_pencil(
+#    times = timeit.repeat(lambda: fast_pencil(
+#        im, element), number=10000, repeat=1)
+#    print('Fast pencil time: {}'.format(min(times)))
+
+        times = timeit.repeat(lambda: orig_pencil(
         im, element), number=10000, repeat=1)
-    print('Fast pencil time: {}'.format(min(times)))
+        print('Orig pencil time: {}'.format(min(times)))
 
-    times = timeit.repeat(lambda: orig_pencil(
-        im, element), number=10000, repeat=1)
-    print('Orig pencil time: {}'.format(min(times)))
-
-    cv2.imshow("penciled", fast)
-    cv2.waitKey(0)
+        cv2.imwrite(os.path.join(penciled_path, file), orig)
+        cv2.imshow("penciled", orig)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
